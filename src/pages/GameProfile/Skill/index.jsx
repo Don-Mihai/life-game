13b8682129db08c868b7a7efdf59329a3c4e7ddf
mikhai.palienko <mikhai.palienko@aliexpress.ru>
2024-10-20 17:39:29 +0300

    level option

--- src/pages/GameProfile/Skill/index.jsx
+++ src/pages/GameProfile/Skill/index.jsx
@@ -1,22 +1,43 @@
 import { IconButton } from '@mui/material';
 import AddIcon from '@mui/icons-material/Add';
-import React from 'react';
+import React, { useState } from 'react';
+import { ContextMenu, ContextMenuItem, ContextMenuTrigger } from 'rctx-contextmenu';
+import styles from './Skill.module.scss';
 
 const Skill = ({ handleLevelClick, skill, addLevelToSkill }) => {
+  const [completedLevels, setCompletedLevels] = useState({});
+
+  const handleMarkAsCompleted = (index) => {
+    setCompletedLevels((prev) => ({
+      ...prev,
+      [index]: true,
+    }));
+  };
+
   return (
-    <div key={skill.name} className="game-profile__skill">
-      <div className="game-profile__skill-name">{skill.name}</div>
-      <div className="game-profile__skill-levels">
-        {skill.levels.map((levelData, i) => (
-          <div key={i} className="game-profile__level" onClick={() => handleLevelClick(skill.name, levelData)}>
-            {levelData.level}
-          </div>
-        ))}
-        <IconButton onClick={() => addLevelToSkill(skill.name)}>
-          <AddIcon />
-        </IconButton>
+    <>
+      <div key={skill.name} className={styles.skill}>
+        <div className={styles.skillName}>{skill.name}</div>
+        <div className={styles.skillLevels}>
+          {skill.levels.map((levelData, i) => (
+            <React.Fragment key={i}>
+              <ContextMenuTrigger id={`context-menu-${skill.name}-${i}`}>
+                <div className={`${styles.level} ${completedLevels[i] ? styles.completed : ''}`} onClick={() => handleLevelClick(skill.name, levelData)}>
+                  {levelData.level}
+                </div>
+              </ContextMenuTrigger>
+              <ContextMenu id={`context-menu-${skill.name}-${i}`}>
+                <ContextMenuItem onClick={() => handleMarkAsCompleted(i)}>Пометить как выполненный</ContextMenuItem>
+              </ContextMenu>
+            </React.Fragment>
+          ))}
+
+          <IconButton onClick={() => addLevelToSkill(skill.name)}>
+            <AddIcon />
+          </IconButton>
+        </div>
       </div>
-    </div>
+    </>
   );
 };
 
