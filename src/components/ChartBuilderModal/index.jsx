f4b05acaa806118cd59aa0d6ecfff5956762e43c
Ruslan <rnagumanov57@gmail.com>
2024-12-05 20:07:59 +0500

    add_log_out

---
+++ src/components/ChartBuilderModal/index.jsx
@@ -0,0 +1,10 @@
+import Modal from '@mui/material/Modal';
+import ChartBuilder from '../ChartBuilder/index.jsx';
+
+export default function ChartBuilderModal({ isOpen, onClose }) {
+  return (
+    <Modal open={isOpen} onClose={onClose} aria-labelledby="modal-modal-title" aria-describedby="modal-modal-description">
+      <ChartBuilder />
+    </Modal>
+  );
+}
---
+++ src/components/MenuButton/index.jsx
@@ -0,0 +1,54 @@
+import * as React from 'react';
+import Button from '@mui/material/Button';
+import Menu from '@mui/material/Menu';
+import MenuItem from '@mui/material/MenuItem';
+
+export default function BasicMenu({ setBuilderEnabled }) {
+  const [anchorEl, setAnchorEl] = React.useState(null);
+
+  const open = Boolean(anchorEl);
+  const handleClick = (event) => {
+    setAnchorEl(event.currentTarget);
+  };
+  const handleClose = () => {
+    setAnchorEl(null);
+  };
+
+  const onClickLogOut = () => {
+    localStorage.removeItem('GAME_USER_ID');
+    window.location.href = '/';
+  };
+
+  return (
+    <div>
+      <Button
+        id="basic-button"
+        aria-controls={open ? 'basic-menu' : undefined}
+        aria-haspopup="true"
+        aria-expanded={open ? 'true' : undefined}
+        onClick={handleClick}
+      >
+        Меню
+      </Button>
+      <Menu
+        id="basic-menu"
+        anchorEl={anchorEl}
+        open={open}
+        onClose={handleClose}
+        MenuListProps={{
+          'aria-labelledby': 'basic-button'
+        }}
+      >
+        <MenuItem
+          onClick={() => {
+            setBuilderEnabled(true);
+            handleClose();
+          }}
+        >
+          Построить График
+        </MenuItem>
+        <MenuItem onClick={onClickLogOut}>Выйти</MenuItem>
+      </Menu>
+    </div>
+  );
+}
