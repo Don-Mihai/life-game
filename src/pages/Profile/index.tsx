a707b3d2e1e9c2fbcf33e78b701721754289ade5
mikhai.palienko <mikhai.palienko@aliexpress.ru>
2024-12-22 14:14:59 +0300

    профиль автозапросы при редаткировании

--- src/pages/Profile/index.tsx
+++ src/pages/Profile/index.tsx
@@ -1,28 +1,52 @@
 import { Avatar, TextField } from '@mui/material';
 // @ts-ignore
 import styles from './Profile.module.scss';
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
+import axios from 'axios';
+import useDebounce from './useDebounce';
 
 enum ProfileFields {
-  NAME = 'fullName',
+  NAME = 'name',
   EMAIL = 'email',
   PASSWORD = 'password'
 }
 
 interface ProfileI {
+  id: string;
   [ProfileFields.NAME]: string;
   [ProfileFields.EMAIL]: string;
   [ProfileFields.PASSWORD]: string;
 }
 
-const initialValues: ProfileI = {
+const initialValues: Omit<ProfileI, 'id'> = {
   [ProfileFields.NAME]: '',
   [ProfileFields.EMAIL]: '',
   [ProfileFields.PASSWORD]: ''
 };
 
+const PROFILE_URL = 'https://6762f51117ec5852cae7acd4.mockapi.io/users/1';
+
 const Profile = () => {
-  const [formValues, setFormValues] = useState<ProfileI>(initialValues);
+  const [formValues, setFormValues] = useState<ProfileI>(initialValues as ProfileI);
+
+  const editUser = () => {
+    axios.put(PROFILE_URL, formValues);
+  };
+
+  const debauncedFormValues = useDebounce(formValues, 1000);
+
+  useEffect(() => {
+    getUser();
+  }, []);
+
+  useEffect(() => {
+    if (debauncedFormValues?.id) editUser();
+  }, [debauncedFormValues]);
+
+  const getUser = async () => {
+    const user: ProfileI = (await axios.get(PROFILE_URL)).data;
+    setFormValues(user);
+  };
 
   const handleChange = (event: React.ChangeEvent<HTMLInputElement | HTMLTextAreaElement>) => {
     const value = event.target.value;
@@ -31,7 +55,7 @@ const Profile = () => {
   };
 
   const clearInputs = () => {
-    setFormValues(initialValues);
+    setFormValues(initialValues as ProfileI);
   };
 
   return (
